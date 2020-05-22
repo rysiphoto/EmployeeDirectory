73282f9cc2a093c479a7f2ea90e7bda0b179beaa
Ryan Siverson <rcsskier@mac.com>
2020-05-21 23:46:01 -0600

    added bootstrap search bar and styling

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -6,13 +6,21 @@ function Title(props) {
     <>
       <div className="jumbotron">
         <h1 className="display-4">Employee Directory</h1>
-        <p className="lead">Search your employee by name! Fun!</p>
+        <p className="lead">Search your employees by name! Fun!</p>
+
+        <nav className="navbar navbar-light bg-danger" id="navbar">
+          <form className="form-inline">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search By Name" aria-label="Search"></input>
+            <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </nav>
       </div>
+
       <div className="list-group list-group-horizontal-lg" id="empTop">
         <div className="list-group-pic"></div>
-        <div className="list-group-item"><h5 className="card-title"><strong>Name</strong></h5>
+        <div className="list-group-name"><h5 className="card-title"><strong>Name</strong></h5>
         </div>
-        <div className="list-group-item"><h5 className="card-title"><strong>Email</strong></h5></div>
+        <div className="list-group-email"><h5 className="card-title"><strong>Email</strong></h5></div>
         <div className="list-group-item"><h5 className="card-title"><strong>Phone</strong></h5>
         </div>
         <div className="list-group-item"><h5 className="card-title"><strong>DOB</strong></h5>
